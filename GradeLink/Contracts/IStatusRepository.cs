6f43df1219e8482c97d698ee7d79340e0baad90e
Gcharles19 <W18JquY712!>
2021-06-15 00:37:40 +0300

    Added Interface Classes

---
+++ GradeLink/Contracts/IStatusRepository.cs
@@ -0,0 +1,13 @@
+﻿using GradeLink.Data;
+using RentRedi.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradeLink.Contracts
+{
+    public interface IStatusRepository : IRepositoryBase<Status>
+    {
+    }
+}
